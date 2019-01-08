3de7b18971ba3667db24abebf9eca0645b044845
Mark T <mdvthomas@gmail.com>
2019-01-07 22:37:54 -0500

    Added browserSync and organized Gulp files

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,13 @@
+var 
+gulp           = require('gulp'),
+postcss        = require('gulp-postcss'),
+autoprefixer   = require('autoprefixer'),
+cssvars        = require('postcss-simple-vars'),
+nested         = require('postcss-nested'),
+cssimport      = require('postcss-import');
+
+gulp.task('styles', function() {
+   return gulp.src('./app/assets/styles/styles.css')
+      .pipe(postcss([cssimport, cssvars, nested, autoprefixer]))
+      .pipe(gulp.dest('./app/temp/styles'));
+});
