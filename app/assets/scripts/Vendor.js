7fc5b26083efb5ac4a3e10e868d944bb2981fb29
Mark T <mdvthomas@gmail.com>
2019-01-21 21:50:51 -0500

    Added browser support fallbacks.

--- app/assets/scripts/Vendor.js
+++ app/assets/scripts/Vendor.js
@@ -1,3 +1,5 @@
 /* packages that work on their own & w/out any supplement code */
 
+import '../../temp/scripts/modernizr';
+import 'picturefill';
 import 'lazysizes';
--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -3,7 +3,7 @@ webpack = require('webpack');
 
 
 /* re-bundles webpack */
-gulp.task('scripts', function(callback) {
+gulp.task('scripts', ['modernizr'], function(callback) {
    webpack(require('../../webpack.config.js'), function(err, stats) {
       if (err) {
          console.log(err.toString());
